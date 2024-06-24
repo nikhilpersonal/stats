c6e65bee170ac545f824e2595c75267e2d4c3516
nikhilpersonal <153951567+nikhilpersonal@users.noreply.github.com>
2024-06-24 18:53:44 -0400

    Add files via upload

---
+++ home.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+
+st.set_page_config(
+    page_title="Betting - Home",
+    page_icon="🏀",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+ 
+
+st.markdown(
+    """
+    ## Howdy Degens
+    Just playing around w some APIs, gonna iterate on this live.
+
+    Current status: just have a frame around the API. You can enter player, stat to track and threshold.
+
+    """
+)
+
+
